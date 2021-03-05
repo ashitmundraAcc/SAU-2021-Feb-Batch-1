81eaa8b9db071d3c7e9e3d690c84a2dd3b4de564
ashitmundraAcc <Ashit.Mundra@accolitedigital.com>
2021-03-05 23:59:46 +0530

    kafka

---
+++ Kafka Morning/LibraryEventBookConsumer/src/main/java/com/au/LibraryEventBookConsumer/OrderEventDeserializer.java
@@ -0,0 +1,27 @@
+package com.au.LibraryEventBookConsumer;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.au.LibraryEventBookConsumer.domain.OrderEvent;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class OrderEventDeserializer implements Deserializer<OrderEvent>{
+
+	@Override
+	public OrderEvent deserialize(String topic, byte[] data) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+		OrderEvent msg = null;
+		try {
+			msg = objectMapper.readValue(data, OrderEvent.class);
+		} catch (Exception e) {
+			System.out.println("Exception while reading value"+e);
+		}
+
+		return msg;
+	}
+
+}
---
+++ Kafka Morning/LibraryEventBookProducer/src/test/java/com/au/LibraryEventBookProducer/LibraryEventBookProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.LibraryEventBookProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventBookProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
